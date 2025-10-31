aff100a74d2f17e977182b970457a09c01f1ee05
Kutay Keleş <79710597+keleskutay@users.noreply.github.com>
2025-10-31 05:41:33 +0300

    (transformer): initialize weights for linar module

--- cs336_basics/linear.py
+++ cs336_basics/linear.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch
+import math
 
 # y = Wx
 class Linear(nn.Module):
@@ -11,5 +12,21 @@ class Linear(nn.Module):
         self.device = device
         self.dtype = dtype
 
+        W = torch.empty((in_features + out_features), device=device dtype=dtype)
+        std = math.sqrt(2.0 / (in_features + out_features))
+
+        min_cutoff = -3 * std
+        max_cutoff = 3 * std
+        
+        # Normalize         N(mean=0, std = (2/ d_input_size + d_output_size))
+        torch.nn.init.trunc_normal_(W, mean = 0, std = std, a = min_cutoff, b = max_cutoff)
+
+        self.weight = nn.Parameter(W)
+
     def forward(self, x: torch.Tensor):
         pass
+
+
+if __name__ == '__main__':
+    test = Linear(1,2)
+    print(test.weight)
